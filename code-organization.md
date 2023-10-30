fcc822ba15a2f7b5d7529037b175ec9d4f05c299
sunny <sun.motiani@gmailcom>
2023-10-30 18:04:03 +0530

    views package

---
+++ code-organization.md
@@ -0,0 +1,28 @@
+#CODE Organization
+
+-Some codebases are easy to navigate
+-Others are hard to follow
+-Code  organization is often the difference 
+
+A good code structure will :
+-Make it easier to guess where bugs are
+-Make it easier to add new features
+-And more
+
+## Flat Structure
+
+All code in single packge 
+Files can still seperate code :
+
+
+```
+myapp/
+    gallery_store.go
+    gallery_handler.go
+    gallery_templates.go
+    user_store.go
+    user_handler.go
+    user_templates.go
+    royter.go
+    ...
+```
--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/sunnymotiani/lenslocked/views"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filepath)
+	t, err := views.Parse(filepath)
 	if err != nil {
-		log.Printf("parsing template : %v", err)
+		log.Printf("parsing template :%v", err)
 		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template : %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
-	}
+	t.Execute(w, nil)
 
 }
 
@@ -42,10 +36,12 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
