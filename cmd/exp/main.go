7bdf345a7409f55be7e9cc7994d144df541214ac
sunny <sun.motiani@gmailcom>
2023-10-31 14:27:35 +0530

    fmt.errorf

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,37 +1,31 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"errors"
+	"fmt"
 )
 
-type User struct {
-	Name string
-	Bio  string
-	Age  int
-}
-
-type UserMeta struct {
-	Visits int
-}
-
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
-	if err != nil {
-		panic(err)
-	}
-
-	user := User{
-		Name: "Sunny Motiani",
-		Bio:  `<script>alert("Hahah you have been hacked")</script>`,
-		Age:  123,
-	}
-
-	err = t.Execute(os.Stdout, user)
-	if err != nil {
-		panic(err)
-	}
-
+	err := CreateOrg()
+	fmt.Println(err)
+}
+func Connect() error {
+	return errors.New("Connection Failed")
 }
 
-//commit
+func CreateUser() error {
+	err := Connect()
+	if err != nil {
+		return fmt.Errorf("create user :%w", err)
+	}
+	//..continue...
+	return nil
+}
+
+func CreateOrg() error {
+	err := CreateUser()
+	if err != nil {
+		return fmt.Errorf("Create Org : %w", err)
+	}
+	return nil
+}
