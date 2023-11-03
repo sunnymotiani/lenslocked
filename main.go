9ce196e436a8b2b0ba958dd1945f0406fa504339
sunny <sun.motiani@gmailcom>
2023-11-03 18:33:20 +0530

    file system created

--- main.go
+++ main.go
@@ -3,23 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sunnymotiani/lenslocked/controllers"
+	"github.com/sunnymotiani/lenslocked/templates"
 	"github.com/sunnymotiani/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))))
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+
 		http.Error(w, "Page Not Found", http.StatusNotFound)
 	})
 	fmt.Println("starting server on :3000...")
