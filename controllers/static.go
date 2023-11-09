7b91ad928458ea77f5f1c451f347eb07e3cdcddb
sunny <sun.motiani@gmailcom>
2023-11-09 13:34:23 +0530

    made faq dynamic

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/sunnymotiani/lenslocked/views"
@@ -9,5 +10,32 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	question := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free Version ?",
+			Answer:   "Yes We do offer a 30 Day Free trial",
+		},
+		{
+			Question: "What Are you support hours ?",
+			Answer:   "24/7",
+		},
+		{
+			Question: "Contact Options ?",
+			Answer:   ` Email us - <a href="mailto:sun.motiani@gmail.com">sun.motiani@gmail.com</a>`,
+		},
+		{
+			Question: "Office Location?",
+			Answer:   "our entire team is remote",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, question)
 	}
 }
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 
