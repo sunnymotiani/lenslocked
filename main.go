0e729b726ce96fdc6e3a0724e104022b2b817f56
sunny <sun.motiani@gmailcom>
2023-11-23 18:06:52 +0530

    added post method

--- main.go
+++ main.go
@@ -13,22 +13,24 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
-
-	r.Get("/contact", controllers.StaticHandler(views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
-
-	r.Get("/faq", controllers.FAQ(views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
-
-	usersC := controllers.Users{}
-	usersC.Templates.New = views.Must(views.ParseFS(
+	r.Get("/", controllers.StaticHandler(views.Must(views.ParseFS(
 		templates.FS,
-		"signup.gohtml", "tailwind.gohtml",
-	))
-	r.Get("/signup", usersC.New)
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		http.Error(w, "Page Not Found", http.StatusNotFound)
-	})
-	fmt.Println("starting server on :3000...")
-	http.ListenAndServe(":3000", r)
+		"home.gohtml", "tailwind.gohtml",
+	))))
+	r.Get("/contact", controllers.StaticHandler(views.Must(
+		views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
+	r.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
+	var usersC controllers.Users
+	usersC.Templates.New = views.Must(views.ParseFS(
+		templates.FS, "signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup", usersC.New)
+	r.Post("/signup", usersC.Create)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
+	fmt.Println("Starting the server on :3000...")
+	http.ListenAndServe(":3000", r)
 }
