e8c4e3e158ce1240fc0b4353ff15275a09a85159
sunny <sun.motiani@gmailcom>
2023-12-18 17:13:57 +0530

    added create in controller

--- controllers/users.go
+++ controllers/users.go
@@ -24,6 +24,12 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Email: ", r.FormValue("email"))
-	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
+	}
+	fmt.Fprint(w, "User Created: %+v", user)
 }
